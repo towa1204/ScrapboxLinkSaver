ed5d95d53f68a296f5dafcc2584732df06886e6c
towa1204 <fdsfgjkal@gmail.com>
2021-02-24 11:30:20 +0900

    currentWindowのみに範囲を限定

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
-chrome.tabs.query({},function(tabs){
+chrome.tabs.query({highlighted: true, currentWindow: true},function(tabs){
   let $textarea = $('<textarea></textarea>');
   let copy_text = '';
   for (let i = 0; i < tabs.length; i++) {
-    if (tabs[i].highlighted === true) {
-      copy_text += '[' + tabs[i].title + ' ' + tabs[i].url + ']\n';
-    }
+    copy_text += '[' + tabs[i].title + ' ' + tabs[i].url + ']\n';
   }
   $textarea.text(copy_text);
   $('#copied').append($textarea);
