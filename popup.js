b75161b2bd409f174655940e07a2f2f36008e42f
towa1204 <fdsfgjkal@gmail.com>
2021-08-07 18:18:49 +0900

    バグ修正

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ chrome.tabs.query({highlighted: true, currentWindow: true},function(tabs){
   let copy_text = '';
   for (let i = 0; i < tabs.length; i++) {
     let newStr = tabs[i].title.replace('[','【').replace(']','】');
-    copy_text += '［' + newStr + ' ' + tabs[i].url + ']\n';
+    copy_text += '[' + newStr + ' ' + tabs[i].url + ']\n';
   }
   $textarea.text(copy_text);
   $('#copied').append($textarea);
