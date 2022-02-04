0dafed79294d2996b2eadc7a6e11a4c17c3d33ea
towa1204 <fdsfgjkal@gmail.com>
2022-02-04 23:38:48 +0900

    複数の[]を【】に変換できるように変更 close #2

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@ chrome.tabs.query({highlighted: true, currentWindow: true},function(tabs){
   let $textarea = $('<textarea></textarea>');
   let copy_text = '';
   for (let i = 0; i < tabs.length; i++) {
-    let newStr = tabs[i].title.replace('[','【').replace(']','】');
-    copy_text += '[' + newStr + ' ' + tabs[i].url + ']\n';
+    let new_str = tabs[i].title.replace(/\[/g,'【').replace(/\]/g,'】');
+    copy_text += '[' + new_str + ' ' + tabs[i].url + ']\n';
   }
   $textarea.text(copy_text);
   $('#copied').append($textarea);
